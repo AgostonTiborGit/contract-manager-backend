a4a3a05276872416b74c4839606b2c91462f6efa
Tibor Ágoston <AgostonTiborGit@users.noreply.github.com>
2026-02-03 11:55:23 +0100

    backend projekt létrehozva

---
+++ src/main/java/hu/agoston/contractmanager/ContractManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package hu.agoston.contractmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractManagerBackendApplication.class, args);
+	}
+
+}
